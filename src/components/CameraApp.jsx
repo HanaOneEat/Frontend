87a277b9b42f7600b57d241f36e588a19c182724
정훈 <wjdgnsxhsl@naver.com>
2024-05-22 13:56:18 +0900

    set Camera done

--- src/components/CameraApp.jsx
+++ src/components/CameraApp.jsx
@@ -25,9 +25,17 @@ const CameraApp = () => {
     <>
       <TopBackNav />
       <div id="CA_container">
-        <img src={CameraBG} alt="카메라 배경" />
+        <div className="title">코드 스캔</div>
         {/* <button onClick={handleStartCamera}>카메라</button> */}
         <video ref={videoRef} autoPlay playsInline></video>
+        <img src={CameraBG} alt="카메라 배경" />
+        <div className="scan_rct_box">
+          <div className="ul_border" />
+          <div className="ur_border" />
+          <div className="dr_border" />
+          <div className="dl_border" />
+        </div>
+        <div className="about">QR코드/바코드를 카메라 전면에 스캔해주세요.</div>
       </div>
     </>
   );
